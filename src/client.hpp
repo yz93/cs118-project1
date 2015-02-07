2f9e2ef4919286ad7d880b0bc8176492f60b6a41
Zhao Yang <yz931129@gmail.com>
2015-02-06 16:35:09 -0800

    replaced my code for part 1 with TA's solution

--- src/client.hpp
+++ src/client.hpp
@@ -26,17 +26,71 @@
 #include "meta-info.hpp"
 
 namespace sbt {
-	
+
 class Client
 {
 public:
-  Client(const std::string& port, const std::string& torrent)
+  class Error : public std::runtime_error
   {
+  public:
+    explicit
+    Error(const std::string& what)
+      : std::runtime_error(what)
+    {
+    }
+  };
+
+public:
+  Client(const std::string& port,
+         const std::string& torrent);
+
+  void
+  run();
+
+  const std::string&
+  getTrackerHost() {
+    return m_trackerHost;
   }
+
+  const std::string&
+  getTrackerPort() {
+    return m_trackerPort;
+  }
+
+  const std::string&
+  getTrackerFile() {
+    return m_trackerFile;
+  }
+
 private:
-	/*MetaInfo m_metaInfo;
-	std::string m_port;
-	std::string m_torrent;*/
+  void
+  loadMetaInfo(const std::string& torrent);
+
+  void
+  connectTracker();
+
+  void
+  sendTrackerRequest();
+
+  void
+  recvTrackerResponse();
+
+private:
+  MetaInfo m_metaInfo;
+  std::string m_trackerHost;
+  std::string m_trackerPort;
+  std::string m_trackerFile;
+
+  uint16_t m_clientPort;
+
+  int m_trackerSock;
+  int m_serverSock = -1;
+
+  fd_set m_readSocks;
+
+  uint64_t m_interval;
+  bool m_isFirstReq;
+  bool m_isFirstRes;
 };
 
 } // namespace sbt
